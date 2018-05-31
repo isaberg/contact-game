d03e3a9df6f8235d3498640134eaa2be66b74fc0
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-05-31 16:49:29 -0400

    added HTML styling and bug-free game logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,201 @@
-const wordArray = ["a", "an", "axe", "army", "attic", "autumn", "antonym", "anaconda", "amendment", "adventurer", "anachronism", "abbreviation", "advertisement","acknowledgment", "agriculturalist"]
-
+const wordArray = ["a", "an", "axe", "army", "attic", "abacus", "antonym", "anaconda", "amendment", "adventurer", "anachronism", "abbreviation", "advertisement","acknowledgment", "agriculturalist"]
 const wordPrompt = ["indefinite article", "the other indefinite article", "attacks odor and trees", "Ain't Really Marines Yet", "haunted part of the house", "calculator before there were numbers", "opposite", "don't want none unless you got buns, hun", "ended Prohibition", "Odysseus was this", "a jetpack in the Dark Ages", "shortened form", "Whassssss upppppp!", "I owe all my skills to General Assembly.", "a farmer"]
 
-var wordLevel = 15
-
-var levelDOM = document.querySelector('.wordLevel')
-var secondsDOM = document.querySelector('.seconds')
-
-var seconds
+const seconds = 10
 var temp
+var timeRemaining
+var wordLevel = 0
+var timer
+var toggle = true
 
-function countdown() {
-    seconds = parseInt(secondsDOM.innerHTML,10)
+const levelDOM = document.querySelector('.wordLevel')
+const secondsDOM = document.querySelector('.seconds')
+const promptDOM = document.querySelector('.promptBox')
+const inputDOM = document.querySelector('#userWord')
+const buttonDOM = document.querySelector('#submit')
+const playDOM = document.querySelector('#play')
+const resetDOM = document.querySelector('#reset')
 
-    if (seconds == 1) {
-      temp = document.querySelector('.seconds')
-      temp.innerHTML = "TIME IS UP"
-      return
-    }
+function reset () {
+  stopInterval()
+  secondsDOM.innerHTML = seconds
+  alert(`Game over! You got to level ${wordLevel}!`)
+}
 
-    seconds = seconds - 1
-    secondsDOM.innerHTML = seconds
-    timeout = setTimeout(countdown, 1000);
+function resetTimer () {
+  secondsDOM.innerHTML = seconds
+}
+
+function togglePlay() {
+  toggle = !toggle
+}
+
+function play () {
+  if (toggle === true) {
+  togglePlay()
+  timer = window.setInterval(interval, 1000)
+  wordLevel = 1
+  console.log(`Game play initiated! wordLevel is ${wordLevel}`)
+  levelDOM.innerHTML = wordLevel
+  promptDOM.innerHTML = wordPrompt[wordLevel-1]
+  }
+  else {
+  alert("You're already playing!")
   }
 
-countdown();
-// how to use this date function...all dates built off of a date 48 years ago...
-// establish reference date
-// establish date + factor (how many milliseconds based upon wordLevel?)
+}
+
+function levelUp () {
+  if (wordLevel < 15) {
+    resetTimer()
+    wordLevel = wordLevel + 1
+    inputDOM.value = ""
+    levelDOM.innerHTML = wordLevel
+    console.log(`${levelDOM.innerHTML} is the level of play.`)
+    promptDOM.innerHTML = wordPrompt[wordLevel-1]
+  }
+  else {
+    reset()
+  }
+}
+
+function wordGrab(e) {
+    e.preventDefault()
+    word = inputDOM.value
+    console.log(`wordGrab'd ${inputDOM.value}`)
+    wordCheck(word)
+}
+
+function wordGrabEnter(e) {
+  if (e.keycode === 13) {
+    e.preventDefault()
+    word = inputDOM.value
+    console.log(`wordGrab'd ${inputDOM.value}`)
+    wordCheck(word)
+  }
+}
+
+function wordCheck (word) {
+  if (word === wordArray[wordLevel-1]) {
+    console.log('Level up activated!')
+    levelUp()
+  }
+  else {
+    alert("Incorrect!")
+  }
+}
+
+
+function interval () {
+  timeRemaining = parseInt(secondsDOM.innerHTML, 10)
+  if(timeRemaining === 0) {
+    reset()
+    secondsDOM.innerHTML = seconds
+  }
+  else {
+      timeRemaining = timeRemaining - 1
+      secondsDOM.innerHTML = timeRemaining
+  }
+}
+
+function stopInterval () {
+    clearTimeout(timer)
+  }
+
+buttonDOM.addEventListener('click', wordGrab)
+inputDOM.addEventListener('keydown', wordGrabEnter)
+playDOM.addEventListener('click', play)
+resetDOM.addEventListener('click', reset)
+
+
+
 /*
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
+function countdown () {
+  let tempSeconds = parseInt(secondsDOM.innerHTML, 10)
 
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+  if (tempSeconds == 0) {
+    temp = document.querySelector('.seconds')
+    temp.innerHTML = "TIME IS UP"
+    reset()
+  }
+  else {
+    tempSeconds = tempSeconds - 1
+    temp = document.querySelector('.seconds')
+    temp.innerHTML = tempSeconds
+    let timer = setTimeout(countdown, 1000)
+  }
+}
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    //document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    //+ minutes + "m " + seconds + "s ";
 
-    // If the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(x);
-      //  document.getElementById("demo").innerHTML = "EXPIRED";
+/////////functional countdown timer
+
+function countdown () {
+  seconds = parseInt(secondsDOM.innerHTML,10)
+
+  if (seconds == 1) {
+    temp = document.querySelector('.seconds')
+    temp.innerHTML = "TIME IS UP"
+    reset()
+    stopCountdown()
+    seconds = seconds + 10
+  }
+  else {
+    seconds = seconds - 1
+    temp = document.querySelector('.seconds')
+    temp.innerHTML = seconds
+    setTimeout(countdown, 1000)
+  }
+}
+
+
+// alternate failing myCountdown
+function countdown () {
+  seconds = 10
+  myCountdown = setTimeout(
+    //why do I need this for countdown to work?
+    seconds = parseInt(secondsDOM.innerHTML,10)
+
+    if (seconds == 0) {
+      temp = document.querySelector('.seconds')
+      temp.innerHTML = "TIME IS UP"
+      stopCountdown()
+      reset()
+      seconds = seconds + 10
     }
-}, 1000);
+    else {
+      seconds = seconds - 1
+      temp = document.querySelector('.seconds')
+      temp.innerHTML = seconds
+    }), 1000)
+}
+//experimental countdown - why fail after Round 1?
 
-/*
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+function countdown () {
+  var tempSeconds = seconds
+  var myCountdown = setTimeout(function() {
+    if (tempSeconds == 0) {
+      temp = document.querySelector('.seconds')
+      temp.innerHTML = 'TIME IS UP'
+      reset()
+      stopCountdown()
+    }
+    else {
+      tempSeconds = tempSeconds - 1
+      temp = document.querySelector('.seconds')
+      temp.innerHTML = tempSeconds
+    }
+  }, 1000)
+}
+
+// fancy display hiding based on conditions (not working yet)
+
+function gameDisplay() {
+  if (wordLevel = 0) {
+    gameMain = document.querySelector('main')
+    gameMain.style.display = "none"
+  }
+}
+
 */
-
-
-//levelUp(): reset timer, add +1 to wordLevel, pull new level wordPrompt
-
-//wordCheck() = on click or Enter or wordTimer =0, check answer against wordArray, if correct, run levelUp(), if wrong reset()
-
-//reset() = wordLevel = 0,
-
-//timer will utilize: window.setTimeout(function, milliseconds)
